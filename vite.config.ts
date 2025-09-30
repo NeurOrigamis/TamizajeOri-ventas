4528b94675f5e24979a99ab64a51f784ff64fd6e
NeurOrigamis <carloscka@gmail.com>
2025-09-30 18:48:24 -0300

    as

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TamizajeOri-ventas/',  // 👈 pon aquí el nombre exacto de tu repo en GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  build: {
+    outDir: 'docs',  // 👈 para que GitHub Pages sirva desde /docs
+  },
+})
