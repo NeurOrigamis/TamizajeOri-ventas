5d8d17883fe67cd06cd16e58d120e720a5c10330
NeurOrigamis <carloscka@gmail.com>
2025-10-05 19:59:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tamizaje-ventas/',   // 
+  base: '/TamizajeOri-ventas/',   // 👈 nombre EXACTO del repo
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
