45a3522af0307413323768565241464f62456436
NeurOrigamis <carloscka@gmail.com>
2025-10-05 21:38:29 -0300

    ac

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TamizajeOri-ventas/', // 👈 nombre EXACTO del repo (con mayúscula O)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
